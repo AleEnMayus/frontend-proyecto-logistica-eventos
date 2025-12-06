12e6c84a1a166ea934ac3e4db7a8d548cc9b7464
Alesim <calejandrito317@gmail.com>
2025-12-06 18:44:57 +0000

    correcion rutas autenticacion

--- src/imports/commonImports.js
+++ src/imports/commonImports.js
@@ -1,9 +1,9 @@
 export { default as HomeGuest } from '../Views/general/HomePageGuest'
 
 // Páginas de autenticación
-export { default as LoginPage } from '../views/general/LogIn'
-export { default as RegisterPage } from '../views/general/Register'
-export { default as RecoverPassword } from '../views/general/RecoverPassword'
+export { default as LoginPage } from '../Views/general/LogIn'
+export { default as RegisterPage } from '../Views/general/Register'
+export { default as RecoverPassword } from '../Views/general/RecoverPassword'
 
 // Páginas de usuario
 export { default as Logout } from '../Views/general/LogOut'
