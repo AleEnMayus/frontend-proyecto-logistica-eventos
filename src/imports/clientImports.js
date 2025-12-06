be05802c5856dbc61e99fa908bed9749f411873b
Alesim <calejandrito317@gmail.com>
2025-12-06 18:46:09 +0000

    correcion rutas cliente

--- src/imports/clientImports.js
+++ src/imports/clientImports.js
@@ -1,16 +1,16 @@
 export { default as NotificationsClient } from '../Views/user/NotificationsClient' 
 export { default as HomeClient } from '../Views/general/HomePageCl'
 // Calendario
-export { default as ClientCalendar } from '../views/user/Calendarclient'
+export { default as ClientCalendar } from '../Views/user/Calendarclient'
 // Eventos
-export { default as EventDetailsC } from '../views/user/Events/EventDetailsCl'
-export { default as ListEventsC } from '../views/user/Events/HomeEventsCl'
+export { default as EventDetailsC } from '../Views/user/Events/EventDetailsCl'
+export { default as ListEventsC } from '../Views/user/Events/HomeEventsCl'
 // Encuesta
-export { default as SurvayClient } from '../views/user/survey'
+export { default as SurvayClient } from '../Views/user/survey'
 // Agendar cita
-export { default as Schedule } from '../views/user/Schedule'
+export { default as Schedule } from '../Views/user/Schedule'
 // Contratos
-export { default as ContractsClient } from '../views/user/Contrats/HomeContratsCl'
+export { default as ContractsClient } from '../Views/user/Contrats/HomeContratsCl'
 // Galería
-export { default as ImageGalleryC } from '../views/user/gallery/galleryC'
-export { default as ImageGalleryViewerC } from '../views/user/gallery/galleryC2'
+export { default as ImageGalleryC } from '../Views/user/gallery/galleryC'
+export { default as ImageGalleryViewerC } from '../Views/user/gallery/galleryC2'
