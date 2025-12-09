3d1cb5204650a58b6e271330290274c85b896d08
Alesim <calejandrito317@gmail.com>
2025-12-09 19:38:20 +0100

    Update EditEvent.jsx

--- src/Views/admin/Events/EditEvent.jsx
+++ src/Views/admin/Events/EditEvent.jsx
@@ -75,7 +75,13 @@ const EditEvent = () => {
       setTimeout(() => navigate(`/EventsHomeAdmin/Details/${eventId}`), 2000);
     } catch (error) {
       console.error("Error actualizando evento:", error);
-      addToast(error.response?.data?.error || error.message || "Error al actualizar el evento", "danger");
+     addToast(
+        error.response?.data?.message ||
+        error.response?.data?.error ||
+        error.message ||
+        "Error al actualizar el evento",
+        "danger"
+      );
     }
   };
 
