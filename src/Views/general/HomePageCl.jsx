400fe3d8fc8f459bad32a645444217ace5263582
Alesim <calejandrito317@gmail.com>
2025-12-09 02:27:19 +0000

    color y hover corregido

--- src/Views/general/HomePageCl.jsx
+++ src/Views/general/HomePageCl.jsx
@@ -226,7 +226,8 @@ const HomeClient = () => {
                     Consulta el manual para aprender a usar la plataforma.
                   </p>
                   <div className="d-flex flex-column gap-2">
-                    <a href="/Documentos/manual_usuario.pdf" target="_blank" rel="noopener noreferrer" className="btn btn-outline-primary btn-sm text-start">
+                    <a href="/Gallery" className="text-decoration-none fw-semibold d-inline-flex align-items-center"
+                    style={{ color:'#15A5E7' }}>
                       <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentcolor" className='me-2'><path d="M760-200H320q-33 0-56.5-23.5T240-280v-560q0-33 23.5-56.5T320-920h280l240 240v400q0 33-23.5 56.5T760-200ZM560-640v-200H320v560h440v-360H560ZM160-40q-33 0-56.5-23.5T80-120v-560h80v560h440v80H160Zm160-800v200-200 560-560Z"/></svg>
                       Manual
                     </a>
